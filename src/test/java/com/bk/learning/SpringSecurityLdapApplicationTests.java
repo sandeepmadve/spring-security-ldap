3bb20304c7d154754e9deede7b6ae367e3a4f90f
Sandeep Madve <sandeep.madve@outlook.com>
2022-08-05 22:50:15 +0530

    Initial commit

---
+++ src/test/java/com/bk/learning/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bk.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
